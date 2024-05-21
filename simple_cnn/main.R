c315158283cf18951de4b461dbee0943a1a265a9
Carson Zhang <carsonzhang4@gmail.com>
2024-05-21 15:38:19 +0200

    removed commented code from main.R

--- simple_cnn/main.R
+++ simple_cnn/main.R
@@ -38,29 +38,3 @@ elapsed_time_mlr3torch = proc.time() - start_time_mlr3torch
 
 print("mlr3torch")
 print(elapsed_time_mlr3torch)
-
-# OLD
-
-# source(here("simple_cnn", "set_up_data.R"))
-
-# start_time_torch <- proc.time()
-# source("learner_torch.R")
-# source("train_torch.R")
-# # source("predict_torch.R")
-# elapsed_time_torch <- proc.time() - start_time_torch
-
-# start_time_mlr3torch <- proc.time()
-# source(here("simple_cnn", "learner_mlr3torch.R"))
-# source(here("simple_cnn", "train_mlr3torch.R"))
-# source("predict_mlr3torch.R")
-# elapsed_time_mlr3torch <- proc.time() - start_time_mlr3torch
-
-# print(paste("Torch: ", elapsed_time_torch, sep = ""))
-# print(paste("mlr3torch: ", elapsed_time_mlr3torch, sep = ""))
-
-# copy the README code
-# i.e. just train on a pre-defined task
-# this will help you understand what the data needs to look like
-# and will ensure that mlr3torch works at all
-# source(here("simple_cnn"), "define_learner.R")
-
