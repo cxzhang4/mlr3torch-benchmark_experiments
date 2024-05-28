e77d29de8fbac850a94c12e3aaba30facbc90e0f
Carson Zhang <carsonzhang4@gmail.com>
2024-05-28 11:08:18 +0200

    pytorch transform syntax is incorrect

--- simple_cnn/mlp/pytorch/data.py
+++ simple_cnn/mlp/pytorch/data.py
@@ -1,6 +1,7 @@
 import torch
 import os
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 from torchvision.transforms import ToTensor
 from torchvision.io import read_image
 import pandas as pd
@@ -10,10 +11,9 @@ val_idx = range(10001, 15000)
 tst_idx = range(15001, 20000)
 
 class GuessTheCorrelationDataset(Dataset):
-    def __init__(self, root, responses_file_path, path, transform=None, target_transform=None):
+    def __init__(self, root, responses_file_path, transform=None, target_transform=None):
         self.root = root
-        self.images = pd.read_csv(responses_file_path)
-        self.path = path
+        self.images = pd.read_csv(root + "/" + responses_file_path)
         self.transform = transform
         self.target_transform = target_transform
 
@@ -23,7 +23,7 @@ class GuessTheCorrelationDataset(Dataset):
     def __getitem__(self, index):
         row = self.images.iloc[index]
 
-        img_path = root + "/" + row["id"].astype(str) + ".png"
+        img_path = self.root + "/train_imgs/" + row["id"] + ".png"
         img = read_image(img_path)
         
         default_transforms = transforms.Compose([
@@ -36,4 +36,6 @@ class GuessTheCorrelationDataset(Dataset):
         if self.transform:
             img = self.transform(img)
 
-        return img
+        label = row["corr"].astype(float)
+
+        return img, label
--- simple_cnn/mlp/pytorch/main.py
+++ simple_cnn/mlp/pytorch/main.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn as nn 
+import torchvision.transforms as transforms
+from data import GuessTheCorrelationDataset
+import time
 
 n_epochs = 3
 batch_size = 64
 lr = 0.01
 
 # create a torch dataset
-
 
 # create a learner
 # TODO: compute input_dim instead of hard-coding it
@@ -24,7 +26,31 @@ learner_torch_mlp = nn.Sequential(
 
 # train the learner
 
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(learner_torch_mlp.parameters(), lr = lr)
+
+transforms_for_corr_images = transforms.Compose([
+    transforms.Lambda(lambda img: img.unsqueeze(1)),
+    transforms.Lambda(lambda img: transforms.functional.crop(img, top = 0, left = 21, height = 131, width = 130))
+])
+
+train_ds = GuessTheCorrelationDataset(root = "data/correlation/guess-the-correlation",
+                                      responses_file_path = "train.csv",
+                                      transform = transforms_for_corr_images,
+                                      )
+
+train_dataloader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
+
+start_time = time.time()
 for i in range(n_epochs):
+    learner_torch_mlp.train()
+    for i, (img, target) in enumerate(train_dataloader):
+        # img, target = img.to(DEVICE), target.to(DEVICE)
+        optimizer.zero_grad()
+        y_pred = learner_torch_mlp(img)
+        loss = loss_fn(y_pred, target)
+        loss.backward()
+        optimizer.step()
+end_time = time.time()
 
-
-# predict using the learner
+print(end_time - start_time)
