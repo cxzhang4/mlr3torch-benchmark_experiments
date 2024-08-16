16a162b39a67c46caaaf3f85075a17a1bd4abb23
Carson Zhang <carson.zhang@campus.lmu.de>
2024-08-16 15:58:09 +0200

    now gets most recent

--- R/output_dir_name.R
+++ R/output_dir_name.R
@@ -1,4 +1,5 @@
 library(here)
+library(stringr)
 
 output_dir_name = function() {
   date_str = format(Sys.Date(), "%d_%m_%Y")
@@ -27,4 +28,10 @@ result_dir_name = function() {
   dir_path = here("results", paste0(date_str, "-", run_num))
   
   dir_path
+}
+
+most_recent_result_name = function() {
+  dirs = list.dirs(path = here("results"), full.names = TRUE)
+  sorted_dirs = str_sort(dirs)
+  sorted_dirs[length(sorted_dirs)]
 }
